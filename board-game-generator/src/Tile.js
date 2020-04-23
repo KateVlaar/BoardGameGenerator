3288ab5768d44da9ab5cdd4810258d9934915bd3
Kate Vlaar <kate.vlaar@shaw.ca>
2020-04-23 16:27:08 -0600

    adding react-color for color picker

--- board-game-generator/src/Tile.js
+++ board-game-generator/src/Tile.js
@@ -4,17 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faFillDrip} from '@fortawesome/free-solid-svg-icons';
 import {faPencilAlt} from '@fortawesome/free-solid-svg-icons';
 import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import { ChromePicker } from 'react-color';
 
 class Tile extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {editingText: false, text: this.props.text};
+        this.state = {
+            editingText: false,
+            changingColor: false,
+            text: this.props.text,
+            background: "powderblue"};
         this.handleChange = this.handleChange.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
     edit = () => {
         console.log('Editing Comment');
         this.setState({editingText: true});
+    }
+
+    colorChange = () => {
+        this.setState({ changingColor: true });
+        console.log(this.state.changingColor);
     }
 
     save = () => {
@@ -24,9 +35,15 @@ class Tile extends React.Component {
     }
 
     onClick = () => {
-        document.querySelector('.Tile').classList.toggle('is-flipped');
-        console.log("HELLO");
+        // if (!this.state.editingText && !this.state.changingColor) {
+        //    document.querySelector('.Tile').classList.toggle('is-flipped');
+        //     console.log('flipped' + this.state.changingColor);
+        // }
     }
+
+    handleColorChangeComplete = (color) => {
+        this.setState({ background: color.hex, changingColor: false });
+    };
 
     handleChange(event) {
         this.setState({text: event.target.value});
@@ -34,19 +51,19 @@ class Tile extends React.Component {
 
     renderTile = () => {
         return (
-            <div className="scene" onClick={this.onClick}>
-                <div className="Tile">
-                    <div className="TileFront TileFace">
-                        <h1 className="label">{this.state.text}</h1>
-                        <button onClick={this.edit} className="tile-button"><FontAwesomeIcon icon={faPencilAlt} size="2x" /></button>
-                        <button className="tile-button"><FontAwesomeIcon icon={faFillDrip} size="2x" /></button>
-                    </div>
-                    <div className="TileBack TileFace">
-                        <h1 className="label">Player must move their marker three places ahead</h1>
-                        <button onClick={this.edit} className="tile-button"><FontAwesomeIcon icon={faPencilAlt} size="2x" /></button>
+                <div className="scene" onClick={this.onClick}>
+                    <div className="Tile">
+                        <div className="TileFront TileFace" draggable>
+                            <h1 className="label">{this.state.text}</h1>
+                            <button onClick={this.edit} className="tile-button"><FontAwesomeIcon icon={faPencilAlt} size="2x" /></button>
+                            <button onClick={this.colorChange} className="tile-button"><FontAwesomeIcon icon={faFillDrip} size="2x" /></button>
+                        </div>
+                        <div className="TileBack TileFace">
+                            <h1 className="label">Player must move their marker three places ahead</h1>
+                            <button onClick={this.edit} className="tile-button"><FontAwesomeIcon icon={faPencilAlt} size="2x" /></button>
+                        </div>
                     </div>
                 </div>
-            </div>
         );
     }
 
@@ -59,9 +76,23 @@ class Tile extends React.Component {
         );
     }
 
+    renderColorChange = () => {
+        return (
+            <div>
+                <div className="Tile">
+                    <h1 className="label">{this.state.text}</h1>
+                    <button onClick={this.handleColorChangeComplete} className="tile-button"><FontAwesomeIcon icon={faCheckCircle} size="2x" /></button>
+                </div>
+                < ChromePicker onChangeComplete={ this.handleColorChangeComplete }/>
+            </div>
+        );
+    }
+
     render() {
         if (this.state.editingText) {
             return this.renderEditText();
+        } else if (this.state.changingColor) {
+            return this.renderColorChange();
         } else {
             return this.renderTile();
         }
