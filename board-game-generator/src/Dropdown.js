77831e911795f243d47b57cf7afecb267a860f1e
Kate Vlaar <kate.vlaar@shaw.ca>
2020-04-25 15:21:05 -0600

    styling for dropdown

--- board-game-generator/src/Dropdown.js
+++ board-game-generator/src/Dropdown.js
@@ -1,6 +1,8 @@
 // Reference: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
 import React from 'react';
 import './Dropdown.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSortDown} from '@fortawesome/free-solid-svg-icons';
 
 class Dropdown extends React.Component {
     constructor(props) {
@@ -18,20 +20,10 @@ class Dropdown extends React.Component {
         document.querySelector('.dropdown-content').classList.toggle("show");
     }
 
-    eachItem = (items) => {
-        return(<a index={items.id}>{items.frontText}</a>);
-    }
-
-    renderItemsFromList = () => {
-        this.props.items.map(function (item) {
-            return(<a index={item.id}>{item.frontText}</a>);
-        })
-    }
-
     render() {
         return (
             <div className="dropdown">
-                <button onClick={this.openDropdown} className="dropdown-button">Dropdown</button>
+                <button onClick={this.openDropdown} className="dropdown-button"><span>Dropdown</span><FontAwesomeIcon className="dropdown-icon" icon={faSortDown} size="2x" /></button>
                 <div className="dropdown-content">
                     {this.props.items.map((item) => {
                         return (<a key={item.id}>{item.frontText}</a>);
